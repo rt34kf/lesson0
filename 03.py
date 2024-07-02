b394326a81b4c732c00340e7cd9f51607a30b2dc
rt34kf <Shchepkina.mika@yandex.ru>
2024-07-02 23:02:48 +0300

    Add files via upload

---
+++ 03.py
@@ -0,0 +1,5 @@
+first = 4
+second = 7
+third = 2
+mean = (first + second +third) / 3
+print(mean)
---
+++ naim.py
@@ -0,0 +1,18 @@
+#"1st program"
+print(9 ** 0.5 * 5)
+#"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1)
+#"3rd program"
+a = 1234
+b = 5678
+print((a % 1000 // 10) + (b % 1000 // 10))
+#"4th program"
+a = 13.42
+b = 42.13
+a1 = int(a)
+b1 = int(b)
+a2 = (13.42 - 13) * 100
+b2 = int((42.13 - 42) * 100)
+print(a1 == b2 or b1 == a2)
+
+
